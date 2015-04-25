d17f86cd94261632c432f9751497c12faafa24db
xyy11070 <547539413@qq.com>
2015-04-25 18:50:17 +0800

    [xyy] 文章正文

--- mobile/article.php
+++ mobile/article.php
@@ -17,7 +17,7 @@ define('IN_ECS', true);
 
 require(dirname(__FILE__) . '/includes/init.php');
 
-$act = !empty($_GET['act']) ? $_GET['act'] : '';
+$act = !empty($_GET['act']) ? $_GET['act'] :'';
 if (!function_exists("htmlspecialchars_decode"))
     {
         function htmlspecialchars_decode($string, $quote_style = ENT_COMPAT)
@@ -32,21 +32,75 @@ if ($act == 'detail')
     $a_id = !empty($_GET['a_id']) ? intval($_GET['a_id']) : '';
     if ($a_id > 0)
     {
-        $article_row = $db->getRow('SELECT title, content FROM ' . $ecs->table('article') . ' WHERE article_id = ' . $a_id . ' AND cat_id > 0 AND is_open = 1');
+        $article_row = $db->getRow('SELECT title, content,author,add_time,source,file_url,description,support_count FROM ' . $ecs->table('article') . ' WHERE article_id = ' . $a_id . ' AND cat_id > 0 AND is_open = 1');
         if (!empty($article_row))
         {
-            $article_row['title'] = encode_output($article_row['title']);
+            
             $replace_tag = array('<br />' , '<br/>' , '<br>' , '</p>');
             $article_row['content'] = htmlspecialchars_decode(encode_output($article_row['content']));
             $article_row['content'] = str_replace($replace_tag, '{br}' , $article_row['content']);
             $article_row['content'] = strip_tags($article_row['content']);
             $article_row['content'] = str_replace('{br}' , '<br />' , $article_row['content']);
+            $article_row['title'] =str_replace($replace_tag, '{br}' , $article_row['title']);;
+            $article_row['title'] =str_replace('{br}', '<br />' , $article_row['title']);;
+            $article_row['add_time'] = date("Y-m-d H:i:s",$article_row['add_time']);
+            $list =$db->getAll('SELECT user_id,content,add_time FROM' .$ecs->table('comment').' WHERE parent_id = '.$a_id .' AND status =1 ');
+            foreach ($list as $k =>$v)
+            {
+                $list[$k]['add_time'] = date("Y-m-d",$v['add_time']);
+            }
+            if(empty($_SESSION['user_id']))
+            {
+                $article_row['login'] = 2;
+            }
+            else 
+            {
+                $article_row['login'] = 1;
+            }
+            $article_row['list'] = $list;
             $smarty->assign('article_data', $article_row);
         }
+        
+       
     }
     $smarty->display('article.html');
 }
 
+else if($act == 'praise')
+{
+    $a_id = !empty($_GET['a_id']) ? intval($_GET['a_id']) : '';
+    if ($a_id > 0)
+    {
+        $res = $db->query(' UPDATE ' .$ecs->table('article') . ' SET support_count = support_count +1  WHERE article_id = ' . $a_id);
+        $count = $db->getOne('SELECT support_count FROM ' . $ecs->table('article') . ' WHERE article_id = ' . $a_id );
+     
+        if($res)
+        {
+            echo $count;
+        }
+        else 
+        {
+            echo '点赞失败！';
+        }
+        
+    }
+}
+else if($act == 'comment')
+{
+    $info =$db->getRow('SELECT user_id,user_name FROM' .$ecs->table('users').' WHERE user_id = '.$_SESSION['user_id'] );
+    $a_id = !empty($_GET['a_id']) ? intval($_GET['a_id']) : '';
+    $a_content = !empty($_GET['content']) ? intval($_GET['content']) : '';
+    $addTime = time();
+    $comment_id = $db->query("INSERT INTO ". $ecs->table('comment'). " (comment_type,user_name,content,add_time,status,parent_id,user_id) VALUES (2,{$info['user_name']},{$a_content},{$addTime},1,{$a_id},{$info['user_id']}".")" );
+    if($comment_id)
+    {
+        echo '评论成功！';
+    }
+    else 
+    {
+        echo '评论失败！';
+    }
+}
 /* 文章列表 */
 else
 {
